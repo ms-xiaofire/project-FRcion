4c162e5611a48e0c145e4255b273570334f3679e
ms-xiaofire <916646626@qq.com>
2018-08-17 18:32:07 +0800

    2018.08.17--1831

---
+++ js/register.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    $('#submit1').click(function () {
+        $('#set1').css('display', 'none');
+        $('#set2').css('display', 'block');
+        $('.bg-white1').css('background-color', '#8e8e8e');
+        $('.color-white1').css('color', '#8e8e8e');
+        $('.bg-white2').css('background-color', '#fff');
+        $('.color-white2').css('color', '#fff');
+    });
+    $('#submit2').click(function () {
+        $('#set2').css('display', 'none');
+        $('#set3').css('display', 'block');
+        $('.bg-white2').css('background-color', '#8e8e8e');
+        $('.color-white2').css('color', '#8e8e8e');
+        $('.bg-white3').css('background-color', '#fff');
+        $('.color-white3').css('color', '#fff');
+    });
+    $('#submit3').click(function () {
+        $('#set3').css('display', 'none');
+        $('#set4').css('display', 'block');
+        $('.bg-white3').css('background-color', '#8e8e8e');
+        $('.color-white3').css('color', '#8e8e8e');
+        $('.bg-white4').css('background-color', '#fff');
+        $('.color-white4').css('color', '#fff');
+    });
+});
