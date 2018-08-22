b78065017df90f778567529fd5eb7c62fb172e25
ms-xiaofire <916646626@qq.com>
2018-08-22 12:05:43 +0800

    2018.08.22--1205

--- js/users.js
+++ js/users.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
-    $('.user-menu>button').click(function () {
-        $(this).addClass('test');
+    //修改密码
+    $('.pwd-left').click(function () {
+        $('.pwd-left').addClass('pwd-button');
+        $('.pwd-right').removeClass('pwd-button');
+        $('.pwd-login').removeClass('pwd-none');
+        $('.pwd-deal').addClass('pwd-none');
     })
+    $('.pwd-right').click(function () {
+        $('.pwd-left').removeClass('pwd-button');
+        $('.pwd-right').addClass('pwd-button');
+        $('.pwd-login').addClass('pwd-none');
+        $('.pwd-deal').removeClass('pwd-none');
+    })
+    // 修改密码 end
 });
