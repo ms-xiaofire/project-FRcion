67835d8096fc23461a97ca578b3c13ff6bd7eb94
ms-xiaofire <916646626@qq.com>
2018-08-23 18:19:00 +0800

    2018.08.23--1818

--- js/profit.js
+++ js/profit.js
@@ -1,3 +1,34 @@
 $(document).ready(function () {
+    //距离某日的倒计时
+    // var endTime=new Date('2018-09-01');
+
+    // 距离今晚24:0:0的倒计时
+    var endYear=new Date().getFullYear();
+    var endMonth=new Date().getMonth()+1;
+    var endDay=new Date().getDate();
+    var endTime2=new Date(endYear,endMonth,endDay);
+
+
+    var leftDiv=function(){
+        var leftTime=endTime2-(new Date().getTime());
+        var leftHours=addNumber(Math.floor(leftTime/1000/60/60%24));
+        var leftMinutes=addNumber(Math.floor(leftTime/1000/60%60));
+        var leftSeconds=addNumber(Math.floor(leftTime/1000%60));
+        document.getElementById("divTime").innerText="倒计时："+leftHours+"时"+leftMinutes+"分"+leftSeconds+"秒";
+        if(leftTime>0){
+            setTimeout(function(){
+                leftDiv();
+            },500);
+        }else{
+            clearTimeout(leftDiv())
+        }
+
+    }
+    leftDiv();
+
+    function addNumber(num){
+        var num=(num>9)?num:('0'+num);
+        return num;
+    }
 
 });
--- js/users.js
+++ js/users.js
@@ -5,12 +5,56 @@ $(document).ready(function () {
         $('.pwd-right').removeClass('pwd-button');
         $('.pwd-login').removeClass('pwd-none');
         $('.pwd-deal').addClass('pwd-none');
-    })
+    });
     $('.pwd-right').click(function () {
         $('.pwd-left').removeClass('pwd-button');
         $('.pwd-right').addClass('pwd-button');
         $('.pwd-login').addClass('pwd-none');
         $('.pwd-deal').removeClass('pwd-none');
-    })
+    });
     // 修改密码 end
+    // 个人信息
+    $('#nickname').click(function () {
+        $('.nickname').hide();
+        $('.nickname-input').show();
+        $('#submit').show();
+    });
+    $('#place').click(function () {
+        $('.place').hide();
+        $('.place-select').show();
+        $('#submit').show();
+    });
+    $('#submit').click(function () {
+        $('.nickname').show();
+        $('.nickname-input').hide();
+        $('.place').show();
+        $('.place-select').hide();
+        $('#submit').hide();
+    });
+    // 个人信息 end
+    //用户中心
+    $('.phone').click(function () {
+        $('.user-dim').addClass('dim');
+        $('#phone').show();
+    });
+    $('.wallet').click(function () {
+        $('.user-dim').addClass('dim');
+        $('#wallet').show();
+    });
+    $('.cancel').click(function () {
+        $('.user-dim').removeClass('dim');
+        $('#phone').hide();
+        $('#wallet').hide();
+    });
+    // 用户中心 end
+    // 邀请好友
+    // 邀请好友 end
+    $('.invite').click(function () {
+        $('.poster-dim').addClass('dim');
+        $('.poster').show();
+    });
+    $('.poster-close').click(function () {
+        $('.poster-dim').removeClass('dim');
+        $('.poster').hide();
+    })
 });
